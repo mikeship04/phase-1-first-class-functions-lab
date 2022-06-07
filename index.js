7b6559218d10a269fc5d9d1561684014942b589a
Mike Shippy <mikeship04@gmail.com>
2022-06-07 14:43:12 -0700

    commit tree 380405263092ba11cce2ca841328f9e9412b4b3b

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+// Assign functions to a variable.
+const returnFirstTwoDrivers = function(anArray) {
+    return anArray.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(anotherArray) {
+    return anotherArray.slice(2, 4)
+}
+
+// Store functions in a data structure.
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// Write functions that return other functions.
+function createFareMultiplier(int) {
+    return function (otherInt) {
+        return int * otherInt;
+    };
+};
+
+// Pass a function to another function.
+const fareDoubler = function(rate) {
+    return rate * 2;
+};
+
+const fareTripler = function(rate) {
+    return rate * 3;
+}
+// Call a function returned by another function.
+const selectDifferentDrivers = function(driverArray, oneFunction) {
+    return oneFunction(driverArray);
+}
